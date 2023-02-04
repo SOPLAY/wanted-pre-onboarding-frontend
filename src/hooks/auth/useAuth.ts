7e08c7ef06faa28a2523792b5dfc5fd79afbd78f
SOPLAY <tkdgns1341@gmail.com>
2023-02-04 18:21:44 +0900

    ✨Feat: add useAuth hooks , authValidate function

---
+++ src/hooks/auth/useAuth.ts
@@ -0,0 +1,35 @@
+import apis from '@apis/index';
+import { useNavigate } from 'react-router-dom';
+import { IReqSign } from 'types/auth.types';
+
+const useAuth = () => {
+  const navigate = useNavigate();
+
+  const signIn = async (data: IReqSign) =>
+    await apis.auth
+      .signIn(data)
+      .then((res) => {
+        localStorage.setItem('token', res.data.access_token);
+        navigate('/todos');
+        return res;
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+
+  const signUp = async (data: IReqSign) =>
+    await apis.auth
+      .signUp(data)
+      .then((res) => {
+        alert('회원가입 성공');
+        navigate('/auth/signin');
+        return res;
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+
+  return { signIn, signUp };
+};
+
+export default useAuth;
