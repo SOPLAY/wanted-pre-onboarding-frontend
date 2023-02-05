8fdbe374f970177df6cd8b89863792335c8091c4
SOPLAY <tkdgns1341@gmail.com>
2023-02-05 21:49:12 +0900

    ♻️Refactor: add logOut ( Header )

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import * as S from './Header.syles';
 import { Link, useLocation } from 'react-router-dom';
+import useAuth from '@hooks/auth/useAuth';
 const Header = () => {
   const location = useLocation();
   const checkAuthLocation = location.pathname.split('/')[1] === 'auth';
-
+  const { logOut } = useAuth();
   return (
     <S.header>
       <S.title>Todos</S.title>
@@ -16,7 +17,9 @@ const Header = () => {
             <Link to='/auth/signup'>signUp</Link>
           )
         ) : (
-          <Link to='/auth/signin'>logOut</Link>
+          <Link to='/auth/signin' onClick={() => logOut()}>
+            logOut
+          </Link>
         )}
       </S.nav>
     </S.header>
