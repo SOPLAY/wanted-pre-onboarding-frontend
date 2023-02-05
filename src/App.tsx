5d5222b7459c7ae7921186429b57c1aa4696a8df
SOPLAY <tkdgns1341@gmail.com>
2023-02-05 22:15:38 +0900

    ♻️Refactor: refactor TodoProvider depth

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import Layout from '@components/layout/Layout';
 import Router from './Router';
 import { BrowserRouter } from 'react-router-dom';
-import TodoProvider from './contexts/todo/TodoProvider';
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
-        <TodoProvider>
-          <Router />
-        </TodoProvider>
+        <Router />
       </Layout>
     </BrowserRouter>
   );
